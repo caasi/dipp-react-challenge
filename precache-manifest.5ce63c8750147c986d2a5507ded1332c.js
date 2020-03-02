fcca9aebfeaf29fd18f46bbdc4229265edbfd98d
caasi Huang <caasi.igd@gmail.com>
2020-03-02 20:11:54 +0800

    Updates

---
+++ precache-manifest.5ce63c8750147c986d2a5507ded1332c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f7226b26b5cdb44cd7e802e1fe8525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83cc1765c23bf7f9dbdd",
+    "url": "/static/css/main.922feb47.chunk.css"
+  },
+  {
+    "revision": "8257d4a22cbd91b1d37f",
+    "url": "/static/js/2.16b06220.chunk.js"
+  },
+  {
+    "revision": "4947a88052be21cc6beaebe2a22afa34",
+    "url": "/static/js/2.16b06220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83cc1765c23bf7f9dbdd",
+    "url": "/static/js/main.fa146b4e.chunk.js"
+  },
+  {
+    "revision": "9b15e0861019753dd00f",
+    "url": "/static/js/runtime-main.f0801d91.js"
+  }
+]);
